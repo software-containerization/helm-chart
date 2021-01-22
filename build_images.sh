afa6dcf157aeb931fcdda170f55222f7fe88fccf
Max <maxvanhasselt@hotmail.nl>
2021-01-22 19:59:58 +0000

    ingress working

--- build_images.sh
+++ build_images.sh
@@ -7,14 +7,15 @@ mkdir temp && cd temp
 git clone https://github.com/software-containerization/todo.git
 
 # Build the image
-sudo docker build -t localhost:32000/todo-api:v1 todo/todo-backend
-
+sudo docker build -t localhost:32000/todo-api:v$1 todo/todo-backend
+sudo docker build -t localhost:32000/todo-frontend:v$1 todo/todo-frontend
 # Enable microk8s registry
 sudo microk8s start
 sudo microk8s enable registry
 
 # Push to the microk8s registry
-sudo docker push localhost:32000/todo-api:v1
+sudo docker push localhost:32000/todo-api:v$1
+sudo docker push localhost:32000/todo-frontend:v$1
 
 echo "Removing temp dir"
 cd .. && rm -rf temp
